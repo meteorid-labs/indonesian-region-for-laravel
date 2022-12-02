442da8de27cd192d89a1289ea547544de75dd1b5
Aslam <iupin5212@gmail.com>
2022-12-02 11:35:49 +0700

    Add example feature test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Meteor\Region\Tests\Feature;
+
+use Meteor\Region\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testRegister()
+    {
+        $this->assertTrue(true);
+    }
+}
